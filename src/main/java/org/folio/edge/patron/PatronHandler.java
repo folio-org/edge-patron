da0a17e36c005f3efd1d2ffd4cdc4ecbc222cc08
gurleenkaurbp <gurleen.kaur2505@gmail.com>
2024-12-18 18:53:12 +0530

    [EDGPATRON-160] - Add put API for /patron/{externalSystemId}

--- src/main/java/org/folio/edge/patron/PatronHandler.java
+++ src/main/java/org/folio/edge/patron/PatronHandler.java
@@ -25,7 +25,7 @@ import java.util.Objects;
 import java.util.TimeZone;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -399,7 +399,7 @@ public class PatronHandler extends Handler {
   }
 
   protected void handleResponse(RoutingContext ctx, HttpResponse<Buffer> resp, String logPrefix,
-                                Function<String, String> errorMessageFunction) {
+                                UnaryOperator<String> errorMessageFunction) {
     HttpServerResponse serverResponse = ctx.response();
 
     int statusCode = resp.statusCode();
